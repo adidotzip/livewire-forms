2cebe60205ad5c4858370f9e84d191d3c314e1c8
adi <im.adityasingh@icloud.com>
2026-04-07 16:59:44 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // 1. This allows you to use the optimized <Image /> component 
+  // for your logo hosted on Framer.
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'framerusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
+  
+  // 2. (Optional) If you want to deploy IMMEDIATELY without 
+  // deleting those unused variables, uncomment the lines below.
+  /*
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  */
+};
 
 export default nextConfig;
